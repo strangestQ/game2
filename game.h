f8d8e632b4293bcefc1849330ec89e30feb0b79a
strangestQ <jimmyoliven@yahoo.co.jp>
2021-11-06 18:07:31 +0900

    数组应用-扫雷

---
+++ game.h
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//游戏的函数的声明
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define EASY_COUNT 10
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+
+//初始化棋盘
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+//打印棋盘
+void DisplayBoard(char board[ROWS][COLS],int row,int col);
+//设置雷
+void SetMine(char mine[ROWS][COLS], int row, int col);
+//排查雷
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
---
+++ test.c
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//扫雷游戏的测试
+
+#include "game.h"
+
+void menu()
+{
+	printf("****************************\n");
+	printf("*******    1.play    *******\n");
+	printf("*******    0.exit    *******\n");
+	printf("****************************\n");
+}
+void game()
+{
+	//创建两个数组，一个存放布置好的雷的信息，一个存放排查出的雷的信息
+	//为了确保棋盘每个坐标都可以查找周围8个坐标，而不越栈
+	//数组的大小应该大上一圈，即创建时，数组的范围分别+2(左右各多一列，上下各多一行)
+	char mine[ROWS][COLS] = { 0 };//布置好的雷的信息,雷对应 - 1，不是雷对应 - 0
+	char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
+	//初始化棋盘
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	//打印棋盘
+	DisplayBoard(show,ROW,COL);
+	//布置雷
+	SetMine(mine, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+	//排查雷
+	//将mine数组排查到的信息传到show数组中
+	FindMine(mine, show, ROW, COL);
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch(input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
